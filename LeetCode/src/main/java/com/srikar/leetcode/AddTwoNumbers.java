2ecac0ceb8360d2afaf157b8d712edaee85b6d7d
Frame02 <Frame02@users.noreply.github.com>
2015-07-31 13:44:55 +0530

    Improved runtime solution accepted

--- LeetCode/src/main/java/com/srikar/leetcode/AddTwoNumbers.java
+++ LeetCode/src/main/java/com/srikar/leetcode/AddTwoNumbers.java
@@ -15,58 +15,67 @@ public class AddTwoNumbers {
         ListNode resultList = new ListNode(0);
         ListNode sumList = resultList;
         int carryForward = 0;
-        int digitsSum = 0;
+        int digitsSum;
+        boolean nextIteration;
 
         while (true) {
-            if (l1 != null && l2 != null) {
-                digitsSum = l1.val + l2.val + carryForward;
+            digitsSum = 0;
+            nextIteration = false;
+            if (l1 != null) {
+                digitsSum += l1.val;
                 l1 = l1.next;
+                if (l1 != null) {
+                    nextIteration = true;
+                }
+            }
+            if (l2 != null) {
+                digitsSum += l2.val;
                 l2 = l2.next;
-            } else if (l1 != null) {
-                digitsSum = l1.val + carryForward;
-                l1 = l1.next;
-            } else if (l2 != null) {
-                digitsSum = l2.val + carryForward;
-                l2 = l2.next;
-            } else {
-                digitsSum = carryForward;
+                if (l2 != null) {
+                    nextIteration = true;
+                }
+            }
+            if (carryForward != 0) {
+                digitsSum += carryForward;
             }
 
             carryForward = digitsSum / 10;
+            if (carryForward != 0) {
+                nextIteration = true;
+            }
             sumList.val = digitsSum % 10;
 
-            if (l1 != null || l2 != null || carryForward != 0) {
+            if (nextIteration) {
                 sumList.next = new ListNode(0);
                 sumList = sumList.next;
             } else {
                 break;
             }
-
         }
         return resultList;
     }
-    
-    private static ListNode buildList(int[] digits){
+
+    private static ListNode buildList(int[] digits) {
         final ListNode rootNode = new ListNode(digits[0]);
         ListNode currentNode = rootNode;
-        for(int i=1; i < digits.length; ++i){
-           currentNode.next = new ListNode(digits[i]);
-           currentNode = currentNode.next;
+        for (int i = 1; i < digits.length; ++i) {
+            currentNode.next = new ListNode(digits[i]);
+            currentNode = currentNode.next;
         }
         return rootNode;
     }
-    
-    public static void main(String[] args){
-        int[] num1 = new int[]{9};
-        int[] num2 = new int[]{8};
-        
+
+    public static void main(String[] args) {
+        int[] num1 = new int[]{0};
+        int[] num2 = new int[]{7,3};
+
         ListNode l1 = buildList(num1);
         ListNode l2 = buildList(num2);
-                
+
         AddTwoNumbers atn = new AddTwoNumbers();
-        ListNode resultList = atn.addTwoNumbers(null, null);
+        ListNode resultList = atn.addTwoNumbers(l1, l2);
         System.out.println("Sum of specified numbers (in reverse order: ");
-        while(resultList != null){
+        while (resultList != null) {
             System.out.print(resultList.val);
             resultList = resultList.next;
         }
