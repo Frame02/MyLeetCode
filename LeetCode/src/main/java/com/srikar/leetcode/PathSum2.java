98e89c64bed4d3b701e66bc600abc4a5ab1fc1ac
Frame02 <Frame02@users.noreply.github.com>
2015-08-11 14:13:22 +0530

    Improved and accepted solution

--- LeetCode/src/main/java/com/srikar/leetcode/PathSum2.java
+++ LeetCode/src/main/java/com/srikar/leetcode/PathSum2.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+For example:
+Given the below binary tree and sum = 22,
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+
  */
 package com.srikar.leetcode;
 
